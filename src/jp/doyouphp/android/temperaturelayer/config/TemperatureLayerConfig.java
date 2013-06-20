5cdff29a30f8a7af60e652e9e23ade123e01844a
Hideyuki Shimooka <shimooka@doyouphp.jp>
2013-06-21 01:01:18 +0900

    added comments

--- src/jp/doyouphp/android/temperaturelayer/config/TemperatureLayerConfig.java
+++ src/jp/doyouphp/android/temperaturelayer/config/TemperatureLayerConfig.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 Hideyuki SHIMOOKA <shimooka@doyouphp.jp>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.doyouphp.android.temperaturelayer.config;
 
 import jp.doyouphp.android.temperaturelayer.R;
@@ -5,6 +21,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+/**
+ * Configuration class
+ *
+ * This class is an interface to access configuration in SharedPreferences.
+ */
 public final class TemperatureLayerConfig {
     public static final String KEY_START_ON_BOOT = "key_start_on_boot";
     public static final String KEY_TEMPERATURE_UNIT = "key_temperature_unit";
